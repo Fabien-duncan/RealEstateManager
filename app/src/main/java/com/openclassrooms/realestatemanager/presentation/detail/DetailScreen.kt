7638820d4088bc553547bbfacc57ef3d3b331645
Fabien Duncan <fabien.duncan@gmail.com>
2023-10-25 13:15:32 +0200

    added an message to the details pas in case there are no images

--- app/src/main/java/com/openclassrooms/realestatemanager/presentation/detail/DetailScreen.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/presentation/detail/DetailScreen.kt
@@ -2,6 +2,7 @@ package com.openclassrooms.realestatemanager.presentation.detail
 
 import android.net.Uri
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +13,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +25,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalSavedStateRegistryOwner
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -28,6 +33,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
+import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.common.utils.TextUtils
 import com.openclassrooms.realestatemanager.data.local.model.Property
 import com.openclassrooms.realestatemanager.domain.model.PropertyModel
@@ -73,6 +79,7 @@ private fun DetailScreenView(
         PropertyPhotosModel("https://designingidea.com/wp-content/uploads/2022/01/modern-home-types-of-room-living-space-dining-area-kitchen-glass-door-floor-rug-loft-pendant-light-ss.jpg", null),
         PropertyPhotosModel("https://foyr.com/learn/wp-content/uploads/2022/05/foyer-or-entry-hall-types-of-rooms-in-a-house-1024x819.jpg", "entrance")
         )
+    //var photos = listOf<PropertyPhotosModel>()
     Column(modifier = modifier.fillMaxSize()) {
 
         Text(
@@ -82,11 +89,16 @@ private fun DetailScreenView(
             color = Color.DarkGray,
             modifier = Modifier.padding(8.dp)
         )
-        LazyRow(){
-            itemsIndexed(photos){index, photo->
-                PhotoItem( photo = photo)
+        if(!photos.isEmpty()) {
+            LazyRow() {
+                itemsIndexed(photos) { index, photo ->
+                    PhotoItem(photo = photo)
+                }
             }
+        }else{
+            EmptyPhotoList()
         }
+
 
 
         Text(text = "Surface Area: ${state.property?.area}")
@@ -133,6 +145,32 @@ fun PhotoItem(
                     textAlign = TextAlign.Center
                 )
             }
+        }
+    }
+}
+@Composable
+fun EmptyPhotoList(){
+    Box(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(120.dp)
+            .clip(MaterialTheme.shapes.extraSmall)
+            .background(MaterialTheme.colorScheme.secondary)
+    ){
+        Column() {
+            Image(
+                painter = painterResource(id = R.drawable.missing_image),
+                contentDescription = null, // Set content description if needed
+                modifier = Modifier.fillMaxWidth().height(80.dp).padding(4.dp)
+            )
+            Text(
+                text = "There are no Images for this Property",
+                color = Color.White,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
