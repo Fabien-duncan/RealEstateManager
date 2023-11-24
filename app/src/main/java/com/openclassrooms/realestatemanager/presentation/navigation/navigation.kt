6008bc33f5f77905ac1eefe323757b6c7ea1009f
Fabien Duncan <fabien.duncan@gmail.com>
2023-11-24 15:36:18 +0100

    the drop down menu to select the currency now works and if you click on the currency you can select the currency. Once selected it will update the value in the sharedPreferences

--- app/src/main/java/com/openclassrooms/realestatemanager/presentation/navigation/navigation.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/presentation/navigation/navigation.kt
@@ -1,11 +1,15 @@
 package com.openclassrooms.realestatemanager.presentation.navigation
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
@@ -14,26 +18,34 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.PopupProperties
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.openclassrooms.realestatemanager.common.ScreenViewState
@@ -80,6 +92,7 @@ fun Navigation(
     Scaffold(
         topBar = {
             TopBar(
+                homeViewModel = homeViewModel,
                 screenType = homeScreenType,
                 onBackArrowPressed = {
                     isItemOpened = it
@@ -278,12 +291,25 @@ fun LaunchDetailScreenFromState(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar(
+    homeViewModel: HomeViewModel,
     screenType: ScreenType,
     onBackArrowPressed: (Boolean) -> Unit,
     onEditPressed: () -> Unit,
     onAddPressed:() -> Unit
 ) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    var expanded by remember { mutableStateOf(false) }
+    var selectedMenuItem by remember { mutableStateOf("Item 1") }
+    var showCurrencyPopup by remember { mutableStateOf(false) }
+    // USA flag emoji
+    val usaFlag = "\uD83C\uDDFA\uD83C\uDDF8"
+    // European Union flag emoji
+    val europeFlag = "\uD83C\uDDEA\uD83C\uDDFA"
+    val currentCurrencyText = when(homeViewModel.getCurrency())
+    {
+        CurrencyType.Euro -> "EUR $europeFlag"
+        CurrencyType.Dollar -> "USD $usaFlag"
+    }
 
     TopAppBar(
         title = {
@@ -292,7 +318,7 @@ fun TopBar(
         navigationIcon = {
             if (screenType == ScreenType.List || screenType == ScreenType.ListWithDetail  ) {
                 IconButton(
-                    onClick = { println("menu pressed") },
+                    onClick = { expanded = true },
                 ) {
                     Icon(
                         imageVector = Icons.Default.Menu,
@@ -301,6 +327,34 @@ fun TopBar(
                     )
 
                 }
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                    modifier = Modifier
+                        .padding(top = 4.dp)
+                        .width(200.dp)
+                ) {
+                    DropdownMenuItem(
+                        onClick = {
+                            selectedMenuItem = "Item 1"
+                            expanded = false
+                            showCurrencyPopup = true
+                        },
+                        text = {
+                            Text(text = "Currency: $currentCurrencyText")
+                        }
+                    )
+                    DropdownMenuItem(
+                        onClick = {
+                            selectedMenuItem = "Item 2"
+                            expanded = false
+                        },
+                        text = {
+                            Text(text = "item2")
+                        }
+                    )
+                }
+
             } else {
                 IconButton(
                     onClick = {
@@ -350,6 +404,71 @@ fun TopBar(
             }
         },
         scrollBehavior = scrollBehavior,
-        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = MaterialTheme.colorScheme.secondary),
+        colors = topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.secondary,
+        ),
     )
+    if (showCurrencyPopup) {
+        CurrencyPopup(
+            homeViewModel = homeViewModel,
+            onDismiss = { showCurrencyPopup = false }
+        )
+    }
+}
+@Composable
+fun CurrencyPopup(
+    homeViewModel: HomeViewModel,
+    onDismiss: () -> Unit
+) {
+
+    Dialog(
+        onDismissRequest = { onDismiss() }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(16.dp)
+        ) {
+            Text("Select Currency")
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = homeViewModel.getCurrency() == CurrencyType.Dollar,
+                        onClick = {
+                            homeViewModel.setCurrency(CurrencyType.Dollar)
+                            onDismiss.invoke()
+                        }
+                    )
+                    Text("USD")
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = homeViewModel.getCurrency() == CurrencyType.Euro,
+                        onClick = {
+                            homeViewModel.setCurrency(CurrencyType.Euro)
+                            onDismiss.invoke()
+                        }
+                    )
+                    Text("EUR")
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = { onDismiss() }) {
+                Text("Close Popup")
+            }
+        }
+    }
 }
